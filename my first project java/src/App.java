6d57cf6e84815b28707b39af6f1d58c0273a849f
Ramon Godoy <ramon.rhg@hotmail.com>
2024-04-26 18:47:34 -0300

    meu primeiro projeto java

---
+++ my first project java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world, I am a little loss, but i wil overcome the difficulties ");
+    }
+}
